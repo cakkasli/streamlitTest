616e813afc2ab7b79c69cb292758f04a7a496301
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-11-28 20:46:01 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -2,24 +2,8 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import matplotlib.pyplot as plt
 
-def generate_drive_download_link(file_url):
-    import re
-
-    # Extract the file ID using regex
-    match = re.search(r'/d/([a-zA-Z0-9_-]+)', file_url)
-    if not match:
-        return "Invalid Google Drive URL"
-    
-    file_id = match.group(1)
-
-    # Construct the direct download link
-    download_link = f"https://drive.google.com/uc?export=download&id={file_id}"
-    return download_link
-
 # Google Sheets URL
-#url = "https://docs.google.com/spreadsheets/d/1Ettpgs-yXvLTPNFNQtJvEMNUcHf2Pa6saqVU0WFkR7k/edit?gid=1310051949#gid=1310051949"
-google_drive_url = "https://drive.google.com/file/d/1-MQjsdPqBiTg2trAzWimGstqQ5vAwFez/view?usp=drive_link"
-url = generate_drive_download_link(google_drive_url)
+url = "https://docs.google.com/spreadsheets/d/1Ettpgs-yXvLTPNFNQtJvEMNUcHf2Pa6saqVU0WFkR7k/edit?gid=1310051949#gid=1310051949"
 
 # Connect to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
