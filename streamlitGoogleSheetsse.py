62c4dd90af50c7fec1398d29326b98ba2cc786df
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-01 19:33:55 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -89,7 +89,7 @@ if data is not None and not data.empty:
         ">
             <img src="https://raw.githubusercontent.com/cakkasli/streamlitTest/refs/heads/main/NORBLIS_LOGO.ico" 
                  alt="NORBLIS Logo" width="40" style="margin-right: 10px;">
-            <span>Laser long-run info SN: {str_serial_num}</span>
+            <span>Laser long-run info <br> SN: {str_serial_num}</span>
         </div>
         """,
         unsafe_allow_html=True,
