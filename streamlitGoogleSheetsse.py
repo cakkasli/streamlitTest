2646f096500c8eea536cd9c5bbbdf44763abb132
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-02 21:43:25 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -175,16 +175,7 @@ if data is not None and not data.empty:
 
     
 
-    # Create a rectangle patch
-    box_width = 0.775  # 80% of the figure width
-    box_height = 0.05  # Height in figure coordinate system
-    box_x = 0.125  # Center the box horizontally
-    box_y = 40.90  # Position the box near the top
-    
-    rect = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
-    
-    # Add the rectangle patch to the figure
-    fig.patches.append(rect)
+ 
 
 
 
@@ -235,7 +226,16 @@ if data is not None and not data.empty:
 
 
 
-
+   # Create a rectangle patch
+    box_width = 0.775  # 80% of the figure width
+    box_height = 0.05  # Height in figure coordinate system
+    box_x = 0.125  # Center the box horizontally
+    box_y = 40.90  # Position the box near the top
+    
+    rect = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
+    
+    # Add the rectangle patch to the figure
+    fig.patches.append(rect)
 
 
     # Add buttons after the plot
