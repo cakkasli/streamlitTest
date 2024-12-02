bdfb7a4a750fbb50ac688b2668aace0d48711403
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-02 21:09:32 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -160,7 +160,7 @@ if data is not None and not data.empty:
                 session_data["ID"].iloc[-1],  # Last ID in the session
                 session_data["ModuleTemperature"].iloc[-1],  # Last value in the session
                 f"{session}", 
-                fontsize=10, fontweight="bold", color=color
+                fontsize=120, fontweight="bold", color=color
         )
         )
 
