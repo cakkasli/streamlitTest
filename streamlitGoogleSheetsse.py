241c714d230d2ea24aeb954297c279a3ebfffbf8
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-03 21:32:59 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -214,7 +214,7 @@ if data is not None and not data.empty:
     # Create a rectangle patch
     box_width = 0.41  # 80% of the figure width
     box_height = 0.05  # Height in figure coordinate system
-    box_x = 0.085  # Center the box horizontally
+    box_x = 0.080  # Center the box horizontally
     box_y = 0.935  # Position the box near the top
     
     rect = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
