08d4e6de0ff42ca84dd6ba029918ef01435624e8
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-05 20:17:45 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -217,7 +217,7 @@ if data is not None and not data.empty:
             )
             axes[0, 1].text(
                 session_data["ID"].iloc[-1],  # Last ID in the session
-                806.5,  # Slightly above the y-axis limit for alignment
+                804.5,  # Slightly above the y-axis limit for alignment
                 "ı", 
                 fontsize=10, fontweight='normal', color='black',
                 ha='center', va='bottom'  # Bottom-align the text
