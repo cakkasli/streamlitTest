ebf2a868d48401701a5d03e60099d76f467a0c6b
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-05 21:33:40 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -230,7 +230,7 @@ if data is not None and not data.empty:
     axes[0, 1].set_ylabel("Pump1 Current [mA]")
 
     # Adjust y-axis limits to ensure the numbers are closer
-    axes[0, 1].set_ylim(600, 800)
+    axes[0, 1].set_ylim(0, 1)
 
 
     # Plot 4: ID vs Pump2Current with session-based colors
