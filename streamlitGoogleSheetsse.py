4541dc64cf0325571b98ca56831b71f794623cae
cakkasli <81676515+cakkasli@users.noreply.github.com>
2024-12-05 21:16:19 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -211,7 +211,7 @@ if data is not None and not data.empty:
         if session % 3 == 1:
             axes[0, 1].text(
                 session_data["ID"].iloc[-1],  # Last ID in the session
-                810.0,  # Slightly above the y-axis limit for alignment
+                809.0,  # Slightly above the y-axis limit for alignment
                 str(session), 
                 fontsize=10, fontweight='bold', color='black',
                 ha='center', va='bottom'  # Bottom-align the text
