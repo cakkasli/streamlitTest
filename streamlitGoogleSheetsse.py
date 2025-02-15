4e76c300a7cd5d3564f7c480ada0aadeeb0f2220
cakkasli <81676515+cakkasli@users.noreply.github.com>
2025-02-15 19:50:48 +0100

    Update streamlitGoogleSheetsse.py

--- streamlitGoogleSheetsse.py
+++ streamlitGoogleSheetsse.py
@@ -7,6 +7,7 @@ import time
 import matplotlib.patches as patches
 import matplotlib.ticker as ticker
 
+st.cache_data()
 
 # Initialize session state keys
 if "password_correct" not in st.session_state:
@@ -33,7 +34,7 @@ def check_password():
         """Checks whether a password entered by the user is correct."""
         if st.session_state["username"] in st.secrets["passwords"] and hmac.compare_digest(
             st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
+            st.secrets.passwords[st.session_state["username"]]
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the username or password.
@@ -62,46 +63,31 @@ def check_password():
     return False
 
 
-
 if not check_password():
     st.stop()
 
 
-# Set the Streamlit page layout to wide (must be the first command)
+# Set the Streamlit page layout to wide
 st.set_page_config(layout="wide")
 
 # Google Sheets URL
 
 # Define the URLs
-url_202203060 = "https://docs.google.com/spreadsheets/d/1acXABDP5REh7SyUuICntxdGzZ0QtD_YPyShohRJGJZU/edit?usp=sharing"
-url_202203061 = "https://docs.google.com/spreadsheets/d/1XVxYCNIWTqMNbdV9waWJSm-ee19CY7LVDd1oSIAMXlk/edit?usp=sharing"
+#url_202203060 = "https://docs.google.com/spreadsheets/d/1aLZykSqi22G89wa2hN7bUMzYcU9oynCCW4Sns_mGYgk/edit?usp=sharing"
+#url_202203061 = "https://docs.google.com/spreadsheets/d/1XVxYCNIWTqMNbdV9waWJSm-ee19CY7LVDd1oSIAMXlk/edit?usp=sharing"
+url_202311117 = "https://docs.google.com/spreadsheets/d/1zNN-bHdiQNPuNBKC1pRslH-gnyAt-OyG--zetovBW9g/edit?usp=sharing"
+url_202203060 = "https://docs.google.com/spreadsheets/d/1ne248c3eLVqmZta4-DcRSQE3blWMELj1ZUEEPKAuZRM/edit?usp=sharing"
 
 # Create a dropdown list in the sidebar to select a URL
 url_options = {
-    "202203060": url_202203060,
-    "202203061": url_202203061
+    "202311117": url_202311117,
+    "202203060": url_202203060
 }
 
-# Add custom CSS for making the options bold in the dropdown
-st.markdown(
-    """
-    <style>
-        /* Target the dropdown options */
-        .stSelectbox div[data-baseweb="select"] div {
-            font-weight: bold !important;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
-with st.sidebar:
-    st.markdown(
-        "<div style='font-weight: bold; color: #264D99; margin-bottom: 0;'>Select a LASER Source</div>",
-        unsafe_allow_html=True
-    )
-    selected_url_key = st.selectbox("", options=list(url_options.keys()))
 
+# Move dropdown to the sidebar
+selected_url_key = st.sidebar.selectbox("Select a LASER Source", options=list(url_options.keys()))
 
 # Set the selected URL based on the dropdown selection
 selected_url = url_options[selected_url_key]
@@ -119,6 +105,23 @@ if data is not None and not data.empty:
                     "PumpTemperature", "Pump1Temperature", "Pump2Temperature", 
                     "ModuleTemperature", "SeedTemperature", "PumpCurrent", 
                     "Pump1Current", "Pump2Current", "OutputPower", "PumpPower"]
+    
+    
+    
+    
+    
+    
+    
+    
+    # Check if data is available
+if data is not None and not data.empty:
+    # Ensure columns have correct names
+    data.columns = ["ID", "SerialNumber", "DateTime", "timeMillisecond", "SessionNumber", 
+                    "PumpTemperature", "Pump1Temperature", "Pump2Temperature", 
+                    "ModuleTemperature", "SeedTemperature", "PumpCurrent", 
+                    "Pump1Current", "Pump2Current", "OutputPower", "PumpPower"]
+    
+
 
 
     
@@ -139,7 +142,7 @@ if data is not None and not data.empty:
             unsafe_allow_html=True,
         )
     
-
+    
     # Rotating icon in the second column
     with col_icon:
         st.markdown(
@@ -157,7 +160,7 @@ if data is not None and not data.empty:
             </style>
             <div class="rotating-icon">
                 <img src="https://raw.githubusercontent.com/cakkasli/streamlitTest/refs/heads/main/NORBLIS_LOGO.ico" 
-                     alt="NORBLIS Logo" width="64">
+                     alt="NORBLIS Logo" width="50">
             </div>
             """,
             unsafe_allow_html=True,
@@ -168,183 +171,223 @@ if data is not None and not data.empty:
     # Add space between the title and the plot
     st.markdown("<div style='height: 30px;'></div>", unsafe_allow_html=True)
     
-   
-    # Get unique session numbers and assign rainbow colors
-    session_numbers = data["SessionNumber"].unique()
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(session_numbers)))
-
     
-    # Create a 2x2 grid of plots
+
+# Filter the data where PumpPower is 16000.0
+filtered_data = data[data["PumpPower"] == 16000]
+filtered_data = data
+
+
+# Define the number of samples per hour
+samples_per_hour = 120
+x_hours = range(len(filtered_data["SeedTemperature"]))
+# Create an array for the x-axis that corresponds to hours
+x_hours_in_hours = [i / samples_per_hour for i in x_hours]  # Convert samples to hours
+
+# Get unique session numbers and assign rainbow colors
+session_numbers = data["SessionNumber"].unique()
+colors = plt.cm.rainbow(np.linspace(0, 1, len(session_numbers)))
+
+
+# Ensure the data has been filtered
+if not filtered_data.empty:
+    # Create a continuous x-axis (ID) and plot all sessions on it
     fig, axes = plt.subplots(2, 2, figsize=(12, 8))  # 2 rows, 2 columns
 
-    # Plot 1: ID vs ModuleTemperature with session-based colors    
+    # Plot 1: ID vs ModuleTemperature (continuous plot)
+    # axes[0, 0].plot(
+    #     x_hours_in_hours,
+    #     filtered_data["ModuleTemperature"], 
+    #     label="Module Temperature", 
+    #     color='blue'
+    # )
+    last_time = 0  # Initialize the last time variable
+    
     for session, color in zip(session_numbers, colors):
         session_data = data[data["SessionNumber"] == session]
+        x_hours = range(len(session_data["ModuleTemperature"]))
+        
+        # Convert samples to hours and adjust for continuity
+        hours = [(i / samples_per_hour) + last_time for i in x_hours]  # Add last_time to ensure continuity
+        
+        # Plot the data for the current session
         axes[0, 0].plot(
-            session_data["ID"], 
+            hours, 
             session_data["ModuleTemperature"], 
             label=f"Session {session}", 
             color=color
         )
         
-
-
-        # Add text annotation for every 6th session with adjusted vertical position
-        if session % 3 == 1:
-            axes[0, 0].text(
-                session_data["ID"].iloc[-1],  # Last ID in the session
-                47.2,  # Slightly above the y-axis limit for alignment
-                str(session), 
-                fontsize=10, fontweight='bold', color='black',
-                ha='center', va='bottom'  # Bottom-align the text
-            )
-            axes[0, 0].text(
-                session_data["ID"].iloc[-1],  # Last ID in the session
-                46.0,  # Slightly above the y-axis limit for alignment
-                "ı", 
-                fontsize=10, fontweight='normal', color='black',
-                ha='center', va='bottom'  # Bottom-align the text
-            )
-
     
-    # Set axis labels
+        # Update last_time to the end of the current session
+        last_time = hours[-1]
+
+        
+
+    # Set labels
+    #axes[0, 0].set_xlabel("Run time [Hour]")
     axes[0, 0].set_ylabel("Module Temperature [°C]", fontweight='bold', fontsize='large')
+    # axes[0, 0].set_ylim(20, 45)
+    # axes[0, 0].set_yticks([20, 25, 30, 35, 40, 45])
+    axes[0, 0].autoscale(axis='y', tight=True)
+    axes[0, 0].yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
+    axes[0, 0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.0f}'))
     
-    # Adjust y-axis limits to ensure the numbers are closer
-    axes[0, 0].set_ylim(20, 45)
-    axes[0, 0].set_yticks([20, 25, 30, 35, 40, 45])
 
-
-    # Plot 2: ID vs SeedTemperature with session-based colors
+    # Plot 2: ID vs SeedTemperature (continuous plot)
+    # axes[1, 0].plot(
+    #     x_hours_in_hours,
+    #     filtered_data["SeedTemperature"], 
+    #     label="Seed Temperature", 
+    #     color='green'
+    # )
+    # axes[1, 0].set_xlabel("Run time [Hour]", fontweight='bold', fontsize='large')
+    # axes[1, 0].set_ylabel("Seed Temperature [°C]", fontweight='bold', fontsize='large')
+    # axes[1, 0].autoscale(axis='y', tight=True)
+    # axes[1, 0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.1f}'))
+    
+    
+    
+    last_time = 0  # Initialize the last time variable
+    
     for session, color in zip(session_numbers, colors):
         session_data = data[data["SessionNumber"] == session]
+        x_hours = range(len(session_data["SeedTemperature"]))
+        
+        # Convert samples to hours and adjust for continuity
+        hours = [(i / samples_per_hour) + last_time for i in x_hours]  # Add last_time to ensure continuity
+        
+        # Plot the data for the current session
         axes[1, 0].plot(
-            session_data["ID"], 
+            hours, 
             session_data["SeedTemperature"], 
             label=f"Session {session}", 
             color=color
         )
-    axes[1, 0].set_xlabel("ID")
+        
+    
+        # Update last_time to the end of the current session
+        last_time = hours[-1]
+        
     axes[1, 0].set_ylabel("Seed Temperature [°C]", fontweight='bold', fontsize='large')
+        # axes[0, 0].set_ylim(20, 45)
+        # axes[0, 0].set_yticks([20, 25, 30, 35, 40, 45])
+    axes[1, 0].autoscale(axis='y', tight=True)
+    axes[1, 0].yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
+    # axes[1, 0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.1f}'))
+    
 
-    # Adjust y-axis limits to ensure the numbers are closer
-    axes[1, 0].set_ylim(26, 28)
-    axes[1, 0].set_yticks([26.0, 26.5, 27.0, 27.5, 28.0])
 
+    # Get the current limits
+    current_ylim = axes[1, 0].get_ylim()
+    
+    # Apply some padding (for example, 10% more space above and below)
+    padding = 0.5 * (current_ylim[1] - current_ylim[0])
+    axes[1, 0].set_ylim(current_ylim[0] - padding, current_ylim[1] + padding)
+    new_ylim_min = current_ylim[0] - padding
+    new_ylim_max = current_ylim[1] + padding
+    axes[1, 0].yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
+    axes[1, 0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.1f}'))
+    axes[1, 0].set_xlabel("Run time [Hour]", fontweight='bold', fontsize='large')
 
-    # Plot 3: ID vs Pump1Current with session-based colors
+    # axes[1, 0].set_ylim(30, 32)
+    # axes[1, 0].set_yticks([30.0, 30.5, 31.0, 31.5, 32.0])
+
+    # Plot 3: ID vs Pump1Current (continuous plot)
+    last_time = 0  # Initialize the last time variable
+    
     for session, color in zip(session_numbers, colors):
         session_data = data[data["SessionNumber"] == session]
+        x_hours = range(len(session_data["Pump1Current"]))
+        
+        # Convert samples to hours and adjust for continuity
+        hours = [(i / samples_per_hour) + last_time for i in x_hours]  # Add last_time to ensure continuity
+        
+        # Plot the data for the current session
         axes[0, 1].plot(
-            session_data["ID"], 
+            hours, 
             session_data["Pump1Current"] / 1000, 
             label=f"Session {session}", 
             color=color
         )
         
-        # Add text annotation for every 6th session with adjusted vertical position
-        if session % 3 == 1:
-            axes[0, 1].text(
-                session_data["ID"].iloc[-1],  # Last ID in the session
-                0.7435,  # Slightly above the y-axis limit for alignment
-                str(session), 
-                fontsize=10, fontweight='bold', color='black',
-                ha='center', va='bottom'  # Bottom-align the text
-            )
-            axes[0, 1].text(
-                session_data["ID"].iloc[-1],  # Last ID in the session
-                0.7415,  # Slightly above the y-axis limit for alignment
-                "ı", 
-                fontsize=10, fontweight='normal', color='black',
-                ha='center', va='bottom'  # Bottom-align the text
-            )
-  
-
+    
+        # Update last_time to the end of the current session
+        last_time = hours[-1]
+        
+    #axes[0, 1].set_xlabel("Run time [Hour]")
     axes[0, 1].set_ylabel("Pump1 Current [A]", fontweight='bold', fontsize='large')
+    # First, autoscale but without the 'tight' option
+    #axes[0, 1].autoscale(axis='y', tight=False)
+    
+    # Get the current limits
+    current_ylim = axes[0, 1].get_ylim()
+    
+    # Apply some padding (for example, 10% more space above and below)
+    padding = 0.5 * (current_ylim[1] - current_ylim[0])
+    axes[0, 1].set_ylim(current_ylim[0] - padding, current_ylim[1] + padding)
+    new_ylim_min = current_ylim[0] - padding
+    new_ylim_max = current_ylim[1] + padding
+    axes[0, 1].yaxis.set_major_locator(ticker.MaxNLocator(nbins=6))
+    axes[0, 1].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.2f}'))
+    
 
-    # Adjust y-axis limits to ensure the numbers are closer
-    axes[0, 1].set_ylim(0.7, 0.74)
-    axes[0, 1].set_yticks([0.70, 0.71, 0.72, 0.73, 0.74])
-    axes[1, 1].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.2f}'))
-
-    # Plot 4: ID vs Pump2Current with session-based colors
+    # Plot 4: ID vs Pump2Current (continuous plot)
+    last_time = 0  # Initialize the last time variable
+    
     for session, color in zip(session_numbers, colors):
         session_data = data[data["SessionNumber"] == session]
+        x_hours = range(len(session_data["Pump2Current"]))
+        
+        # Convert samples to hours and adjust for continuity
+        hours = [(i / samples_per_hour) + last_time for i in x_hours]  # Add last_time to ensure continuity
+        
+        # Plot the data for the current session
         axes[1, 1].plot(
-            session_data["ID"], 
-            session_data["Pump2Current"] / 1000, 
+            hours, 
+            session_data["Pump2Current"] /1000, 
             label=f"Session {session}", 
             color=color
         )
-    axes[1, 1].set_xlabel("ID")
+        
+    
+        # Update last_time to the end of the current session
+        last_time = hours[-1]
+    
+    
+    axes[1, 1].set_xlabel("Run time [Hour]", fontweight='bold', fontsize='large')
     axes[1, 1].set_ylabel("Pump2 Current [A]", fontweight='bold', fontsize='large')
+    # axes[1, 1].set_ylim(9.6, 10.0)
+    # axes[1, 1].set_yticks([9.6, 9.7, 9.8, 9.9, 10.0])
+    axes[1, 1].autoscale(axis='y', tight=False)
 
-    axes[1, 1].set_yticks([2.0, 4.0, 6.0, 8.0, 10.0])
-    # Format the y-axis ticks to show one decimal place
+    # Get the current limits
+    current_ylim = axes[1, 1].get_ylim()
+    
+    # Apply some padding (for example, 10% more space above and below)
+    padding = 0.5 * (current_ylim[1] - current_ylim[0])
+    axes[1, 1].set_ylim(current_ylim[0] - padding, current_ylim[1] + padding)
+    new_ylim_min = current_ylim[0] - padding
+    new_ylim_max = current_ylim[1] + padding
+    axes[1, 1].yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
     axes[1, 1].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{x:.1f}'))
-    
-
-    # Create a rectangle patch
-    box_width = 0.41  # 80% of the figure width
-    box_height = 0.040  # Height in figure coordinate system
-
-    box_x = 0.075  # Center the box horizontally
-    box_y = 0.900  # Position the box near the top
-    
-    rect_left = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
-
-    #box_x = 0.568  # Center the box horizontally
-    
-    #rect_right = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
-
-    # Add the rectangle patch to the figure
-    fig.patches.append(rect_left)
-    #fig.patches.append(rect_right)
-
-    axes[0, 0].text(
-    3650,  # Last ID in the session
-    49.5,  # Slightly above the y-axis limit for alignment
-    "Session Number", 
-    fontsize=11, fontweight='bold', color='black',
-    ha='center', va='bottom'  # Bottom-align the text
-    )
-    
-    
-    # Create a rectangle patch
-    box_width = 0.41  # 80% of the figure width
-    box_height = 0.040  # Height in figure coordinate system
-
-    box_x = 0.568  # Center the box horizontally
-    box_y = 0.900  # Position the box near the top
-    
-    rect_right = patches.Rectangle((box_x, box_y), box_width, box_height, linewidth=1, edgecolor='black', facecolor='white', alpha=0.5, transform=fig.transFigure)
-
-    # Add the rectangle patch to the figure
-    fig.patches.append(rect_right)
-    
-    axes[0, 1].text(
-    3650,  # Last ID in the session
-    0.747,  # Slightly above the y-axis limit for alignment
-    "Session Number", 
-    fontsize=11, fontweight='bold', color='black',
-    ha='center', va='bottom'  # Bottom-align the text
-    )
-
 
     # Adjust layout for better spacing
     fig.tight_layout(pad=2.0)  # Add padding between subplots
 
     # Display the plots in Streamlit
     st.pyplot(fig)
-
+      
+    
+    
     # Add buttons after the plot
     col1, col2, col3 = st.columns([1, 1, 1])  # Create three equally spaced columns
-
+    
     with col1:
         if st.button("Clear Cache"):
             st.cache_data.clear()
             st.success("Cache cleared!")
-
+    
     with col2:
         if st.button("Log Off"):
             # Reset session state
@@ -352,15 +395,15 @@ if data is not None and not data.empty:
             st.session_state["username"] = None
             st.session_state["password"] = None
             st.session_state["logoff"] = False  # Reset the logoff flag
-
+    
             # Provide feedback to the user
             st.success("You have been logged off successfully! Redirecting...")
             time.sleep(1)  # Wait 1 second for feedback to be visible
             st.rerun()  # Rerun to clear the interface
-
+    
             # Stop execution
             st.stop()
-
+    
     with col3:
         # Add a button to download the data as a CSV
         if data is not None and not data.empty:  # Ensure there's data to download
@@ -371,7 +414,8 @@ if data is not None and not data.empty:
                 file_name="data.csv",
                 mime="text/csv",
             )
-        else:
-            st.warning("No data available to download.")
+
+
 else:
-    st.warning("No data available to plot.")
+    st.warning("No data available to plot where PumpPower is 16000.0.")
+
